71a30139d7676841b21d479d87305d17b6d2484f
firazyn <fyn99.178@gmail.com>
2021-01-01 15:53:55 +0700

    corrected migrations

--- app/Database/Migrations/2021-01-01-023617_users.php
+++ app/Database/Migrations/2021-01-01-023617_users.php
@@ -1,43 +1,50 @@
-<?php namespace App\Database\Migrations;
+<?php
+
+namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
 class Users extends Migration
 {
-	$this->forge->addField([
+	public function up()
+	{
 
-		'id' => [
-			'type' => 'INT',
-			'constraint' => 11,
-			'unsigned' => TRUE,
-			'auto_increment' => TRUE
-		],
-		'username' => [
-			'type' => 'VARCHAR',
-			'constraint' => 100,
-		],
-		'fullname' => [
-			'type' => 'VARCHAR',
-			'constraint' => 100,
-		],
-		'password' => [
-			'type' => 'TEXT',
-			'constraint' => '100',
-		],
-		'created_at' => [
-			'type' => 'DATETIME',
-			'null' => TRUE
-		],
-		'update_at' => [
-			'type' => 'DATETIME',
-			'null' => TRUE
-		],
+		$this->forge->addField([
+
+			'id' => [
+				'type' => 'INT',
+				'constraint' => 11,
+				'unsigned' => TRUE,
+				'auto_increment' => TRUE
+			],
+			'username' => [
+				'type' => 'VARCHAR',
+				'constraint' => 100,
+			],
+			'fullname' => [
+				'type' => 'VARCHAR',
+				'constraint' => 100,
+			],
+			'password' => [
+				'type' => 'TEXT',
+				'constraint' => '100',
+			],
+			'created_at' => [
+				'type' => 'DATETIME',
+				'null' => TRUE
+			],
+			'update_at' => [
+				'type' => 'DATETIME',
+				'null' => TRUE
+			],
 
 
-	]);
-	$this->forge->addKey('id', TRUE);
-	$this->forge->createTable('users');
-}
+		]);
+
+
+		$this->forge->addKey('id', TRUE);
+		$this->forge->createTable('users');
+	}
 
 
 	//--------------------------------------------------------------------
--- app/Database/Migrations/2021-01-01-025824_order.php
+++ app/Database/Migrations/2021-01-01-025824_order.php
@@ -6,46 +6,51 @@ use CodeIgniter\Database\Migration;
 
 class Order extends Migration
 {
-	$this->forge->addField([
 
-		'id_order' => [
-			'type' => 'INT',
-			'constraint' => 11,
-			'unsigned' => TRUE,
-			'auto_increment' => TRUE
-		],
-		'nama_konsumen' => [
-			'type' => 'VARCHAR',
-			'constraint' => 100,
-		],
-		'alamat' => [
-			'type' => 'TEXT',
-			'constraint' => '100',
-		],
-		'nama_pakaian' => [
-			'type' => 'VARCHAR',
-			'constraint' => 100,
-		],
-		'jumlah_pakaian' => [
-			'type' => 'INT',
-			'constraint' => 100,
-		],
-		'penjemputan' => [
-			'type' => 'VARCHAR',
-			'constraint' => 100,
-		],
-		'total_harga' => [
-			'type' => 'INT',
-			'constraint' => 11,
-			'unsigned' => TRUE,
-			'auto_increment' => TRUE
-		],
+	public function up()
+	{
+
+		$this->forge->addField([
+
+			'id_order' => [
+				'type' => 'INT',
+				'constraint' => 11,
+				'unsigned' => TRUE,
+				'auto_increment' => TRUE
+			],
+			'nama_konsumen' => [
+				'type' => 'VARCHAR',
+				'constraint' => 100,
+			],
+			'alamat' => [
+				'type' => 'TEXT',
+				'constraint' => '100',
+			],
+			'nama_pakaian' => [
+				'type' => 'VARCHAR',
+				'constraint' => 100,
+			],
+			'jumlah_pakaian' => [
+				'type' => 'INT',
+				'constraint' => 100,
+			],
+			'penjemputan' => [
+				'type' => 'VARCHAR',
+				'constraint' => 100,
+			],
+			'total_harga' => [
+				'type' => 'INT',
+				'constraint' => 11,
+				'unsigned' => TRUE,
+				'auto_increment' => TRUE
+			],
 
 
-	]);
-	$this->forge->addKey('id', TRUE);
-	$this->forge->createTable('order');
-}
+		]);
+
+		$this->forge->addKey('id', TRUE);
+		$this->forge->createTable('order');
+	}
 
 
 	//--------------------------------------------------------------------
