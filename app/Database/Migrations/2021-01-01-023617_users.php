9eeda7d9a1a041a4674438b467a581c73b6f50aa
ElshintaMilenia <48706960+ElshintaMilenia@users.noreply.github.com>
2020-12-31 23:58:42 -0800

    add migration

---
+++ app/Database/Migrations/2021-01-01-023617_users.php
@@ -0,0 +1,49 @@
+<?php namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Users extends Migration
+{
+	$this->forge->addField([
+
+		'id' => [
+			'type' => 'INT',
+			'constraint' => 11,
+			'unsigned' => TRUE,
+			'auto_increment' => TRUE
+		],
+		'username' => [
+			'type' => 'VARCHAR',
+			'constraint' => 100,
+		],
+		'fullname' => [
+			'type' => 'VARCHAR',
+			'constraint' => 100,
+		],
+		'password' => [
+			'type' => 'TEXT',
+			'constraint' => '100',
+		],
+		'created_at' => [
+			'type' => 'DATETIME',
+			'null' => TRUE
+		],
+		'update_at' => [
+			'type' => 'DATETIME',
+			'null' => TRUE
+		],
+
+
+	]);
+	$this->forge->addKey('id', TRUE);
+	$this->forge->createTable('users');
+}
+
+
+	//--------------------------------------------------------------------
+
+	public function down()
+	{
+		$this->forge->dropTable('users');
+	}
+}
---
+++ app/Database/Migrations/2021-01-01-025824_order.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Order extends Migration
+{
+	$this->forge->addField([
+
+		'id_order' => [
+			'type' => 'INT',
+			'constraint' => 11,
+			'unsigned' => TRUE,
+			'auto_increment' => TRUE
+		],
+		'nama_konsumen' => [
+			'type' => 'VARCHAR',
+			'constraint' => 100,
+		],
+		'alamat' => [
+			'type' => 'TEXT',
+			'constraint' => '100',
+		],
+		'nama_pakaian' => [
+			'type' => 'VARCHAR',
+			'constraint' => 100,
+		],
+		'jumlah_pakaian' => [
+			'type' => 'INT',
+			'constraint' => 100,
+		],
+		'penjemputan' => [
+			'type' => 'VARCHAR',
+			'constraint' => 100,
+		],
+		'total_harga' => [
+			'type' => 'INT',
+			'constraint' => 11,
+			'unsigned' => TRUE,
+			'auto_increment' => TRUE
+		],
+
+
+	]);
+	$this->forge->addKey('id', TRUE);
+	$this->forge->createTable('order');
+}
+
+
+	//--------------------------------------------------------------------
+
+	public function down()
+	{
+		$this->forge->dropTable('order');
+	}
+}
